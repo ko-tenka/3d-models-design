b68c83e6ff6ce1a0b7d7ccc18bce23fb9858a8d0
ko-tenka <kotelnikova123.123@gmail.com>
2025-06-13 18:15:12 +0300

    release pleloader

---
+++ src/components/Preloader/Preloader.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Preloader.css'
+
+export default function Preloader() {
+  return (
+    <div>
+    <div class="preloader">
+      <div class="frame">
+        <div class="center">
+          <div class="ball"></div>
+          <div class="blubb-1"></div>
+          <div class="blubb-2"></div>
+          <div class="blubb-3"></div>
+          <div class="blubb-4"></div>
+          <div class="sparkle-1"></div>
+          <div class="sparkle-2"></div>
+          <div class="sparkle-3"></div>
+          <div class="sparkle-4"></div>
+          <div class="sparkle-5"></div>
+        </div>
+      </div>
+    </div>
+    </div>
+  )
+}
--- src/ui/uiploshadka/uiploshadka.tsx
+++ src/ui/uiploshadka/uiploshadka.tsx
@@ -1,11 +1,31 @@
-import Spline from "@splinetool/react-spline";
+import React, { useEffect, useState } from 'react';
+import Spline from '@splinetool/react-spline';
 import './uiploshadka.css';
+import Preloader from '../../components/Preloader/Preloader'; // Импортируй свой компонент Preloader
 
 export default function Uiploshadka() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const loadScene = async () => {
+      const response = await fetch("https://prod.spline.design/q1UWbLW6CaAWNCse/scene.splinecode");
+      if (response.ok) {
+        setIsLoaded(true);
+      }
+    };
+    loadScene();
+  }, []);
+  
   return (
     <div className="uiploshadka__wrapper">
-      <Spline scene="https://prod.spline.design/q1UWbLW6CaAWNCse/scene.splinecode" />
-      <div className="uiploshadka__text"></div>
+      {/* Пока сцена не загрузится, показываем прелоадер */}
+      {isLoaded ? (
+        <Spline scene="https://prod.spline.design/q1UWbLW6CaAWNCse/scene.splinecode" />
+      ) : (
+        <Preloader /> // Показать твой preloader, пока сцена не загрузится
+      )}
+    <div className="uiploshadka__text"></div>
     </div>
   );
 }
+
