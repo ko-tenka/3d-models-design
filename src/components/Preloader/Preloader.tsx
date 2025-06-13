a2ec12a4f97cd2e8428f835ec6f8ed9bf877e49a
ko-tenka <kotelnikova123.123@gmail.com>
2025-06-13 18:19:15 +0300

    release fix import

---
+++ src/components/Preloader/Preloader.tsx
@@ -0,0 +1,26 @@
+// Preloader.tsx
+import React from 'react'
+import './Preloader.css'
+
+export default function Preloader() {
+  return (
+    <div className="preloader">
+      <div className="frame">
+        <div className="center">
+          <div className="ball"></div>
+          <div className="blubb-1"></div>
+          <div className="blubb-2"></div>
+          <div className="blubb-3"></div>
+          <div className="blubb-4"></div>
+          <div className="sparkle-1"></div>
+          <div className="sparkle-2"></div>
+          <div className="sparkle-3"></div>
+          <div className="sparkle-4"></div>
+          <div className="sparkle-5"></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
